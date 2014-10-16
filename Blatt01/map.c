60ee94ec8b07681d8a9634c8d61aaf966faefa25
Fabian Behrendt <l-fabian-l@Fabian-ThinkPad-L430-Ubuntu.(none)>
2014-10-16 15:57:01 +0200

    Inital

---
+++ Blatt01/map.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+// Definieren Sie ein enum cardd
+typedef enum {
+	N = 1,
+	E = 2,
+	S = 4,
+	W = 8,
+} cardd;
+
+// Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
+cardd map[3][3];
+
+// Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
+// Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
+// Überprüfen Sie außerdem dir auf Gültigkeit
+void set_dir (int x, int y, cardd dir)
+{
+	if(0 <= x && x < 3  &&  0 <= y && y < 3)
+	{
+		if(dir & N || dir & E || dir & S || dir & W)
+		{
+			map[x][y] = dir;
+		}
+	}
+}
+
+// Die Funktion show_map soll das Array in Form einer 3x3-Matrix ausgeben
+void show_map (void)
+{
+	int i,j;
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			if(j!= 0)
+			{
+				printf("   ");
+			}
+
+			cardd d = map[i][j];
+			
+			switch (d)
+			{
+				case N:
+					printf("%c", 'N');
+					break;
+				case E:
+					printf("%c", 'E');
+					break;
+				case S:
+					printf("%c", 'S');
+					break;
+				case W:
+					printf("%c", 'W');
+					break;
+				default:
+					printf("%c", '0');
+			}
+		}
+
+		printf("\n");
+	}
+}
+
+int main (void)
+{
+	// In dieser Funktion darf nichts verändert werden!
+	set_dir(0, 1, N);
+	set_dir(1, 0, W);
+	set_dir(1, 4, W);
+	set_dir(1, 2, E);
+	set_dir(2, 1, S);
+
+	set_dir(0, 0, N|W);
+	set_dir(0, 2, N|E);
+	set_dir(0, 2, N|S);
+	set_dir(2, 0, S|W);
+	set_dir(2, 2, S|E);
+	set_dir(2, 2, E|W);
+
+	show_map();
+
+	return 0;
+}
---
+++ Makefile
@@ -0,0 +1,33 @@
+NAMES=BackBehrendt
+MAILS=3back 3behrend
+
+INF=informatik.uni-hamburg.de
+COLOR=\e[0;32m
+NC=\e[0m
+CLEAN=find -mindepth 2 -name Makefile -execdir make clean \;
+TAR=tar --exclude=*.swp --exclude=*.pdf --transform='s|$(NUM:/=)|$(NAMES)|' -czvf $(NAMES).tar.gz $(NUM)
+
+all: cleansubdirs package send
+
+cleansubdirs:
+	@$(CLEAN) > /dev/null
+
+help:
+	@echo "Usage: make NUM=«Verzeichnis»"
+
+package:
+ifndef NUM
+	$(error NUM is not defined. See make help)
+endif
+
+	@echo -e "$(COLOR)Creating Tar archive$(NC)"
+	@$(TAR) > /dev/null 
+	 
+send:
+	@echo -e "$(COLOR)Sending mail via thunderbird$(NC)"
+	@thunderbird -compose "to=hr-abgabe@wr.$(INF),subject=$(NAMES),cc='$(foreach M,$(MAILS),$(M)@$(INF),)',attachment=$(shell pwd)/$(NAMES).tar.gz,body=Moin%2C%0Aanbei%20unsere%20Abgabe%0A%0AGruß%0A$(NAMES)"
+
+clean:
+	@echo -e "$(COLOR)Cleaning directory$(NC)"
+	@$(RM) $(NAMES).tar.gz
+
